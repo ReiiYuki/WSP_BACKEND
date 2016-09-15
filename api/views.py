91267f4703f295aa6c9c692be43776536aa0eadc
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-09-15 21:54:44 +0700

    Test some user

--- api/views.py
+++ api/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
+    customer = User.objects.create_user('jodasdsaddddhn', 'lennon@thebeatles.com', 'johnpassword',first_name='John',last_name='Lenon')
+    print (customer.first_name,customer.last_name,customer.email)
+
     return HttpResponse("Test API")
