fa732042809113a9b10095109891026add3f7e0e
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-09-16 12:44:30 +0700

    CORS

--- WSP_BACKEND/settings.py
+++ WSP_BACKEND/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -124,3 +126,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_ALLOW_ALL = True
--- api/views.py
+++ api/views.py
@@ -9,15 +9,38 @@ from django.http import HttpResponse
 class UserViewSet(viewsets.ModelViewSet):
     queryset= User.objects.all()
     serializer_class  = UserSerializer
+
+    ''' Login
+        1. Get request data
+        2. Authenticate is user existed with this username and password
+        3. Check is it None
+        3.1 if not None Login this user and response "S" for succesing login
+        3.2 if None response "F" for Fail to Client
+    '''
     @list_route(methods=['post'], url_path='login')
     def login(self, request):
-        print (request.data)
         username = request.data['username']
         password = request.data['password']
         user = authenticate(username=username, password=password)
         if user is not None :
             login(request,user)
             if request.user.is_authenticated() :
-                print ('%s is logged in to server...')
+                print ('%s is logged in to server...'%request.user.username)
                 return HttpResponse("S", content_type="text/plain")
         return HttpResponse("F", content_type="text/plain")
+
+    # # @login_required
+    # @list_route(methods=['put'],url_path='change-password')
+    # def change_password(self,request) :
+    #     print (request.user)
+    #     # old_password = request.data['old_password']
+    #     # new_password = request.data['new_password']
+    #     # user = authenticate(username=request.user.username,password=old_password)
+    #     # if user is not None :
+    #     #     user.set_password(new_password)
+    #     #     user.save()
+    #     #     login(user)
+    #     #     if request.user.is_authenticated() :
+    #     #         print ('%s is succesing changing password in to server...'%request.user.username)
+    #     #         return HttpResponse("S", content_type="text/plain")
+    #     return HttpResponse("F", content_type="text/plain")
