9b9698b4f56969ce158cf14eea3c90640ff91fd1
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-10-03 15:52:03 +0700

    Initialize Django project

--- api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url,include
-from . import views
-from rest_framework.routers import DefaultRouter
-
-router = DefaultRouter()
-router.register(r'users', views.UserViewSet)
-urlpatterns = [
-    url(r'^', include(router.urls)),
-]
---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\yukir\workspace\python\naturaldrinkbackend\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
