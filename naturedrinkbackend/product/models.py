279c06b5f9df2b53f016c6180f135fb1449e32e8
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-10-18 20:28:57 +0700

    Product Model

--- naturedrinkbackend/product/models.py
+++ naturedrinkbackend/product/models.py
@@ -5,3 +5,10 @@ class Category(models.Model) :
     name = models.CharField(max_length=50)
     description = models.TextField()
     is_active = models.BooleanField(default=True)
+
+class Product(models.Model) :
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    is_active = models.BooleanField(default=True)
+    price = models.FloatField()
+    category = models.ForeignKey(Category,related_name='product')
