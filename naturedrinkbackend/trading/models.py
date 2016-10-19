85fe092c3ff1d19eabd7174dbb3708deb79be10b
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-10-19 15:41:52 +0700

    Payment Method Model

--- naturedrinkbackend/trading/models.py
+++ naturedrinkbackend/trading/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class PaymentMethod(models.Model) :
+    type = models.CharField(max_length=1)
+    name = models.CharField(max_length=13)
+    is_active = models.BooleanField(default=True)
