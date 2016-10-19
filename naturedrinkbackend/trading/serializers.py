21fa7f989425fcc3f9bc4343b4698d8bdf16ecf6
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-10-19 20:01:57 +0700

    PaymentMethod Serializer

--- naturedrinkbackend/trading/serializers.py
+++ naturedrinkbackend/trading/serializers.py
@@ -1,2 +1,8 @@
 from rest_framework import serializers
 from .models import PaymentMethod , ItemProperty , Order , ItemLine , PostalTrack
+
+class PaymentMethodSerializer(serializers.ModelSerializer) :
+    class Meta :
+        model = PaymentMethod
+        fields = ('id','type','name','is_active')
+        extra_kwargs = {'id':{'read_only':True},'is_active':{'read_only':True}}
