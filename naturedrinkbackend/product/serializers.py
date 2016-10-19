57c613e3e1df7a2cef8b7abe497124ada8a74e6f
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-10-19 12:43:43 +0700

    Option/Choice Serializer

--- naturedrinkbackend/product/serializers.py
+++ naturedrinkbackend/product/serializers.py
@@ -1,11 +1,25 @@
 from rest_framework import serializers
-from .models import Category,Product
+from .models import Category,Product,ProductOption,ProductChocie
+
+class ProductChoiceSerializer(serializers.ModelSerializer) :
+    class Meta :
+        model = ProductChocie
+        fields = ('id','name','is_active','option')
+        extra_kwargs = {'id':{'read_only':True},'is_active':{'read_only':True}}
+
+class ProductOptionSerializer(serializers.ModelSerializer) :
+    choices = ProductChocieSerializer(many=True)
+    class Meta :
+        model = ProductOption
+        fields = ('id','name','is_active','product','choices')
+        extra_kwargs = {'id':{'read_only':True},'is_active':{'read_only':True},'choices':{'read_only':True}}
 
 class ProductSerializer(serializers.ModelSerializer) :
+    options = ProductOptionSerializer(many=True)
     class Meta :
         model = Product
-        fields = ('id','name','description','price','is_active','category')
-        extra_kwargs = {'id':{'read_only':True},'is_active':{'read_only':True}}
+        fields = ('id','name','description','price','is_active','category','options')
+        extra_kwargs = {'id':{'read_only':True},'is_active':{'read_only':True},'options':{'read_only':True}}
 
 class CategorySerializer(serializers.ModelSerializer) :
     products = ProductSerializer(many=True)
