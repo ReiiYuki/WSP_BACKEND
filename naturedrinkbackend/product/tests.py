ec44295eec5b77fa7bdae9d675501a23fef61bdf
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-11-02 00:41:11 +0700

    Test GET product

--- naturedrinkbackend/product/tests.py
+++ naturedrinkbackend/product/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase,APIClient
+from rest_framework import status
+from . import models
+import json
+class ProductTest(APITestCase) :
 
-# Create your tests here.
+    def setUp(self) :
+        self.category = models.Category.objects.create(name='Sample',description='Sample')
+        self.product = models.Product.objects.create(name='P',description='PP',price=9,category=self.category)
+
+    def test_list_product(self) :
+        response = self.client.get('/api/v1/p/product/')
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+        self.assertEqual(json.loads(json.dumps(response.data)),[{"name":"P","description":"PP","price":9,"category":self.category.id,"is_active":True,"image":"","id":self.product.id}])
+
+    def test_get_product(self) :
+        response = self.client.get('/api/v1/p/product/'+str(self.product.id)+'/')
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+        self.assertEqual(response.data,{"name":"P","description":"PP","price":9,"category":self.category.id,"is_active":True,"image":"","id":self.product.id})
+
+class Category(APITestCase) :
+
+    def test_list_category(self) :
+        response = self.client.get('/api/v1/p/category/')
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
--- naturedrinkbackend/user/tests.py
+++ naturedrinkbackend/user/tests.py
@@ -1,3 +1,178 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase,APIClient
+from rest_framework import status
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from .models import Address
+class UserTest(APITestCase) :
 
-# Create your tests here.
+    def test_register_user(self):
+        data = {"username":"test","password":"test","first_name":"test","last_name":"test","email":"test@test.test"}
+        response = self.client.post('/api/v1/u/user/',data,format="json")
+        self.assertEqual(response.status_code,status.HTTP_201_CREATED)
+
+    def test_incomplete_register_user(self):
+        data = {"password":"test","first_name":"test","last_name":"test","email":"test@test.test"}
+        response = self.client.post('/api/v1/u/user/',data,format="json")
+        self.assertEqual(response.status_code,status.HTTP_400_BAD_REQUEST)
+        data = {"username":"test","first_name":"test","last_name":"test","email":"test@test.test"}
+        response = self.client.post('/api/v1/u/user/',data,format="json")
+        self.assertEqual(response.status_code,status.HTTP_400_BAD_REQUEST)
+
+    def test_login_user(self):
+        data = {"username":"test","password":"test","first_name":"test","last_name":"test","email":"test@test.test"}
+        response = self.client.post('/api/v1/u/user/',data,format="json")
+        data={"username":"test","password":"test"}
+        response = self.client.post('/api/v1/u/login/',data,format="json")
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+        token = Token.objects.get(user__username='test')
+        self.assertEqual(response.data['token'],token.key)
+
+    def test_login_fail_user(self):
+        data={"username":"test","password":""}
+        response = self.client.post('/api/v1/u/login/',data,format="json")
+        self.assertEqual(response.status_code,status.HTTP_400_BAD_REQUEST)
+
+    def test_get_user_anonymous(self):
+        response = self.client.get('/api/v1/u/user/0/')
+        self.assertEqual(response.status_code,status.HTTP_401_UNAUTHORIZED)
+
+    def test_get_user(self) :
+        data = {"username":"test","password":"test","first_name":"test","last_name":"test","email":"test@test.test"}
+        response = self.client.post('/api/v1/u/user/',data,format="json")
+        data={"username":"test","password":"test"}
+        response = self.client.post('/api/v1/u/login/',data,format="json")
+        token = response.data['token']
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token)
+        response = self.client.get('/api/v1/u/user/0/')
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+        self.assertEqual(response.data,{'id':response.data['id'],'username':'test','is_active':True,'first_name':'test','last_name':'test','email':'test@test.test'})
+
+    def test_change_password(self):
+        data = {"username":"test","password":"test","first_name":"test","last_name":"test","email":"test@test.test"}
+        response = self.client.post('/api/v1/u/user/',data,format="json")
+        data={"username":"test","password":"test"}
+        response = self.client.post('/api/v1/u/login/',data,format="json")
+        token = response.data['token']
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token)
+        data = {"password":"test","new_password":"A"}
+        response = self.client.put('/api/v1/u/user/change_password/',data,type="json")
+        data={"username":"test","password":"test"}
+        response = self.client.post('/api/v1/u/login/',data,format="json")
+        self.assertEqual(response.status_code,status.HTTP_400_BAD_REQUEST)
+        data={"username":"test","password":"A"}
+        response = self.client.post('/api/v1/u/login/',data,format="json")
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+
+    def test_edit_user(self) :
+        data = {"username":"test","password":"test","first_name":"test","last_name":"test","email":"test@test.test"}
+        response = self.client.post('/api/v1/u/user/',data,format="json")
+        data={"username":"test","password":"test"}
+        response = self.client.post('/api/v1/u/login/',data,format="json")
+        token = response.data['token']
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token)
+        response = self.client.get('/api/v1/u/user/0/')
+        id = response.data['id']
+        data = {"first_name":"A","last_name":"B","email":"A@B.C"}
+        response = self.client.put(('/api/v1/u/user/'+str(id)+'/edit/'),data,format="json")
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token)
+        response = self.client.get('/api/v1/u/user/0/')
+        self.assertEqual(response.data['first_name'],"A")
+        self.assertEqual(response.data['last_name'],"B")
+        self.assertEqual(response.data['email'],"A@B.C")
+
+    def test_is_admin_user(self) :
+        user = User.objects.create(username="a",password="b",is_staff=True,is_superuser=True)
+        token = Token.objects.get(user__username='a')
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+        response = self.client.get('/api/v1/u/user/is_admin/')
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+
+    def test_is_not_admin(self):
+        data = {"username":"test","password":"test","first_name":"test","last_name":"test","email":"test@test.test"}
+        response = self.client.post('/api/v1/u/user/',data,format="json")
+        data={"username":"test","password":"test"}
+        response = self.client.post('/api/v1/u/login/',data,format="json")
+        token = response.data['token']
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token)
+        response = self.client.get('/api/v1/u/user/is_admin/')
+        self.assertEqual(response.status_code,status.HTTP_401_UNAUTHORIZED)
+
+class AddressTest(APITestCase):
+
+    def setUp(self) :
+        self.user = User.objects.create(username="a",password="b")
+        self.token = "Token "+Token.objects.get(user__username='a').key
+        self.client.credentials(HTTP_AUTHORIZATION=self.token)
+        self.anonymous_client = APIClient()
+
+    def test_add_address(self) :
+        data = {"address_number":"57/138","village":"Thiptanee","road":"Latphrao","sub_distinct":"Chandrasem","distinct":"Chatujak","province":"Bangokok","country":"Thailand","zipcode":"10900"}
+        response = self.client.post('/api/v1/u/address/',data,format="json")
+        self.assertEqual(response.status_code,status.HTTP_201_CREATED)
+
+    def test_fail_add_address(self) :
+        data = {"village":"Thiptanee","road":"Latphrao","sub_distinct":"Chandrasem","distinct":"Chatujak","province":"Bangokok","country":"Thailand","zipcode":"10900"}
+        response = self.client.post('/api/v1/u/address/',data,format="json")
+        self.assertEqual(response.status_code,status.HTTP_400_BAD_REQUEST)
+        data = {"address_number":"57/138","road":"Latphrao","sub_distinct":"Chandrasem","distinct":"Chatujak","province":"Bangokok","country":"Thailand","zipcode":"10900"}
+        response = self.client.post('/api/v1/u/address/',data,format="json")
+        self.assertEqual(response.status_code,status.HTTP_400_BAD_REQUEST)
+        data = {"address_number":"57/138","village":"Thiptanee","sub_distinct":"Chandrasem","distinct":"Chatujak","province":"Bangokok","country":"Thailand","zipcode":"10900"}
+        response = self.client.post('/api/v1/u/address/',data,format="json")
+        self.assertEqual(response.status_code,status.HTTP_400_BAD_REQUEST)
+        data = {"address_number":"57/138","village":"Thiptanee","road":"Latphrao","distinct":"Chatujak","province":"Bangokok","country":"Thailand","zipcode":"10900"}
+        response = self.client.post('/api/v1/u/address/',data,format="json")
+        self.assertEqual(response.status_code,status.HTTP_400_BAD_REQUEST)
+        data = {"address_number":"57/138","village":"Thiptanee","road":"Latphrao","sub_distinct":"Chandrasem","province":"Bangokok","country":"Thailand","zipcode":"10900"}
+        response = self.client.post('/api/v1/u/address/',data,format="json")
+        self.assertEqual(response.status_code,status.HTTP_400_BAD_REQUEST)
+        data = {"address_number":"57/138","road":"Latphrao","sub_distinct":"Chandrasem","distinct":"Chatujak","country":"Thailand","zipcode":"10900"}
+        response = self.client.post('/api/v1/u/address/',data,format="json")
+        self.assertEqual(response.status_code,status.HTTP_400_BAD_REQUEST)
+        data = {"address_number":"57/138","road":"Latphrao","sub_distinct":"Chandrasem","distinct":"Chatujak","province":"Bangokok","zipcode":"10900"}
+        response = self.client.post('/api/v1/u/address/',data,format="json")
+        self.assertEqual(response.status_code,status.HTTP_400_BAD_REQUEST)
+        data = {"address_number":"57/138","road":"Latphrao","sub_distinct":"Chandrasem","distinct":"Chatujak","province":"Bangokok","country":"Thailand"}
+        response = self.client.post('/api/v1/u/address/',data,format="json")
+        self.assertEqual(response.status_code,status.HTTP_400_BAD_REQUEST)
+
+    def test_get_list_address(self) :
+        data = {"address_number":"57/138","village":"Thiptanee","road":"Latphrao","sub_distinct":"Chandrasem","distinct":"Chatujak","province":"Bangokok","country":"Thailand","zipcode":"10900"}
+        response = self.client.post('/api/v1/u/address/',data,format="json")
+        id = response.data['id']
+        response = self.client.get('/api/v1/u/address/')
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+        self.assertEqual(response.data,[
+        {"id":id,"address_number":"57/138","village":"Thiptanee","road":"Latphrao","sub_distinct":"Chandrasem","distinct":"Chatujak","province":"Bangokok","country":"Thailand","zipcode":"10900","is_active":True}
+        ])
+
+    def test_get_address(self) :
+        data = {"address_number":"57/138","village":"Thiptanee","road":"Latphrao","sub_distinct":"Chandrasem","distinct":"Chatujak","province":"Bangokok","country":"Thailand","zipcode":"10900"}
+        response = self.client.post('/api/v1/u/address/',data,format="json")
+        id = response.data['id']
+        response = self.client.get('/api/v1/u/address/'+str(id)+'/')
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+        self.assertEqual(response.data,{"id":id,"address_number":"57/138","village":"Thiptanee","road":"Latphrao","sub_distinct":"Chandrasem","distinct":"Chatujak","province":"Bangokok","country":"Thailand","zipcode":"10900","is_active":True})
+
+    def test_get_address_anonymous(self) :
+        data = {"address_number":"57/138","village":"Thiptanee","road":"Latphrao","sub_distinct":"Chandrasem","distinct":"Chatujak","province":"Bangokok","country":"Thailand","zipcode":"10900"}
+        response = self.client.post('/api/v1/u/address/',data,format="json")
+        id = response.data['id']
+        response = self.anonymous_client.get('/api/v1/u/address/'+str(id)+'/')
+        self.assertEqual(response.status_code,status.HTTP_401_UNAUTHORIZED)
+
+    def test_list_address_anonymous(self) :
+        data = {"address_number":"57/138","village":"Thiptanee","road":"Latphrao","sub_distinct":"Chandrasem","distinct":"Chatujak","province":"Bangokok","country":"Thailand","zipcode":"10900"}
+        response = self.client.post('/api/v1/u/address/',data,format="json")
+        response = self.anonymous_client.get('/api/v1/u/address/')
+        self.assertEqual(response.status_code,status.HTTP_401_UNAUTHORIZED)
+
+    def test_update_address(self) :
+        data = {"address_number":"57/138","village":"Thiptanee","road":"Latphrao","sub_distinct":"Chandrasem","distinct":"Chatujak","province":"Bangokok","country":"Thailand","zipcode":"10900"}
+        response = self.client.post('/api/v1/u/address/',data,format="json")
+        id = response.data['id']
+        self.assertEqual(response.data,{"id":id,"address_number":"57/138","village":"Thiptanee","road":"Latphrao","sub_distinct":"Chandrasem","distinct":"Chatujak","province":"Bangokok","country":"Thailand","zipcode":"10900","is_active":True})
+        data = {"address_number":"57","village":"Thip","road":"Lat","sub_distinct":"Chan","distinct":"Cha","province":"Ban","country":"Tha","zipcode":"10"}
+        response = self.client.put('/api/v1/u/address/'+str(id)+'/',data,format="json")
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+        self.assertEqual(response.data,{"address_number":"57","village":"Thip","road":"Lat","sub_distinct":"Chan","distinct":"Cha","province":"Ban","country":"Tha","zipcode":"10","is_active":True,"id":id})
