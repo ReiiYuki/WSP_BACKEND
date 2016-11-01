78e7b688bdef337e27d8b635ba52ff5d2dc1b984
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-11-01 21:20:26 +0700

    register_test

---
+++ naturedrinkbackend/user/test.py
@@ -0,0 +1,11 @@
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.contrib.auth.models import User
+
+class UserTest(APITestCase) :
+    def test_register_user(self):
+        data = {"username":"test","password":"test","first_name":"test","last_name":"test","email":"test@test.test"}
+        response = self.client.post('/api/v1/u/user/',data,format="json")
+        self.assertEqual(response.status_code,status.HTTP_201_CREATED)
+        self.assertEqual(User.objects.count(),1)
+        self.assertEqual(User.objects.get().first_name,'test')
