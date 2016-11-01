316fcfa6563950adab64c423ac6a815993e2c72d
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-11-01 20:59:46 +0700

    Test Setting

--- naturedrinkbackend/naturedrinkbackend/settings.py
+++ naturedrinkbackend/naturedrinkbackend/settings.py
@@ -137,6 +137,12 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication'
+    ),
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json',
+    'TEST_REQUEST_RENDERER_CLASSES': (
+        'rest_framework.renderers.MultiPartRenderer',
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.TemplateHTMLRenderer'
     )
 }
 
