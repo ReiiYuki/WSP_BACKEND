4cd69b9bcc304b4622bc689c90376be2eb9af009
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-11-01 21:30:19 +0700

    test incomplete register form

--- naturedrinkbackend/user/test.py
+++ naturedrinkbackend/user/test.py
@@ -9,3 +9,12 @@ class UserTest(APITestCase) :
         self.assertEqual(response.status_code,status.HTTP_201_CREATED)
         self.assertEqual(User.objects.count(),1)
         self.assertEqual(User.objects.get().first_name,'test')
+
+    def test_incomplete_register_user(self):
+        data = {"password":"test","first_name":"test","last_name":"test","email":"test@test.test"}
+        response = self.client.post('/api/v1/u/user/',data,format="json")
+        self.assertEqual(response.status_code,status.HTTP_400_BAD_REQUEST)
+        data = {"username":"test","first_name":"test","last_name":"test","email":"test@test.test"}
+        response = self.client.post('/api/v1/u/user/',data,format="json")
+        self.assertEqual(response.status_code,status.HTTP_400_BAD_REQUEST)
+    
