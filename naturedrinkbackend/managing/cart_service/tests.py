6922d2c0c8460764d277b704394137ec57837734
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-11-02 12:09:36 +0700

    Test Update PaymentMethod

---
+++ naturedrinkbackend/managing/cart_service/tests.py
@@ -0,0 +1,37 @@
+from rest_framework.test import APITestCase,APIClient
+from rest_framework import status
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from trading.models import ItemLine,PaymentMethod
+
+class PaymentMethodTest(APITestCase):
+    def setUp(self) :
+        self.user = User.objects.create(username='admin',password='admin',is_staff=True)
+        self.client.credentials(HTTP_AUTHORIZATION="Token "+Token.objects.get(user__username='admin').key)
+
+    def test_create_payment_method(self) :
+        data = {
+            "name" : "1103702001392",
+            "type" : "B",
+            "is_active" : True
+        }
+        response = self.client.post('/api/v1/m/method/',data,format="json")
+        self.assertEqual(response.status_code,status.HTTP_201_CREATED)
+
+    def test_update_payment_method(self) :
+        data = {
+            "name" : "1103702001392",
+            "type" : "B",
+            "is_active" : True
+        }
+        response = self.client.post('/api/v1/m/method/',data,format="json")
+        id = response.data['id']
+        self.assertEqual(response.status_code,status.HTTP_201_CREATED)
+        data = {
+            "name" : "110-702-013-2",
+            "type" : "B",
+            "is_active" : True
+        }
+        response = self.client.put('/api/v1/m/method/'+str(id)+'/',data,format="json")
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+        self.assertEqual(response.data['name'],data['name'])
--- naturedrinkbackend/managing/tests.py
+++ naturedrinkbackend/managing/tests.py
@@ -1,4 +1,4 @@
-# from .cart_service.tests import PaymentMethodTest,ItemLineTest
+from .cart_service.tests import PaymentMethodTest#,ItemLineTest
 # from .order_service.tests import OrderTest
 from .product_service.tests import CategoryTest,ProductTest
 from .user_service.tests import UserTest
