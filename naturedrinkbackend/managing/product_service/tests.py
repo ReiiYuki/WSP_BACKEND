52e227ece1f988dbfe30e9804aadebebc4190fb7
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-11-02 10:17:09 +0700

    deactive category

--- naturedrinkbackend/managing/product_service/tests.py
+++ naturedrinkbackend/managing/product_service/tests.py
@@ -26,3 +26,16 @@ class CategoryTest(APITestCase) :
     def test_get_category(self):
         response = self.client.get('/api/v1/m/category/')
         self.assertEqual(response.status_code,status.HTTP_200_OK)
+
+    def test_deactivve_category(self) :
+        data={"name":"Drink","description":"Small Molecule"}
+        response = self.client.post('/api/v1/m/category/',data,format="json")
+        self.assertEqual(response.status_code,status.HTTP_201_CREATED)
+        response = self.client.get('/api/v1/m/category/')
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+        id = str(response.data[0]['id'])
+        response = self.client.delete('/api/v1/m/category/'+str(response.data[0]['id'])+'/')
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+        response = self.client.get('/api/v1/m/category/'+id+'/')
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+        self.assertEqual(response.data['is_active'],False)
