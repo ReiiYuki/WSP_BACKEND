042046516cbc8f6fdfd3b924c8c723778b0946da
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-11-02 07:39:44 +0700

    Test get order

--- naturedrinkbackend/trading/tests.py
+++ naturedrinkbackend/trading/tests.py
@@ -113,6 +113,6 @@ class OrderTest(APITestCase) :
         response = self.client.post('/api/v1/t/cart/',data,format="json")
         data = {"method":self.paymentMethod.id,"address":self.address.id}
         response = self.client.post('/api/v1/t/cart/pay/',data,format="json")
-        response = self.client.get('/api/v1/t/order/'+response.data['id']+'/')
+        response = self.client.get('/api/v1/t/order/'+str(response.data['id'])+'/')
         self.assertEqual(response.status_code,status.HTTP_200_OK)
-        self.assertEqual(response.data,{'method':self.paymentMethod.id,'id':response.data[0]['id'],'address':self.address.id,'create_date':response.data[0]['create_date'],'last_update_date':response.data[0]['last_update_date'],'transfer_slip':response.data[0]['transfer_slip'],'is_paid':False,'is_shipped':False,'user':self.user.id,'is_active':True,'postal_track':response.data[0]['postal_track'],'status':'Wait for slip'})
+        self.assertEqual(response.data,{'method':self.paymentMethod.id,'id':response.data['id'],'address':self.address.id,'create_date':response.data['create_date'],'last_update_date':response.data['last_update_date'],'transfer_slip':response.data['transfer_slip'],'is_paid':False,'is_shipped':False,'user':self.user.id,'is_active':True,'postal_track':response.data['postal_track'],'status':'Wait for slip'})
