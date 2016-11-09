c52d781224bcf1e36255e576a251c708f295b410
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-11-09 13:51:33 +0700

    Fix bug on Order Serializer

--- naturedrinkbackend/managing/order_service/serializers.py
+++ naturedrinkbackend/managing/order_service/serializers.py
@@ -4,5 +4,5 @@ class OrderSerializer(serializers.ModelSerializer) :
     status = serializers.ReadOnlyField()
     class Meta :
         model = Order
-        fields = ('id','method','address','create_date','last_update_date','transfer_slip','is_paid','is_shipped','user','is_active','postal_track','status')
+        fields = ('id','method','address','create_date','transfer_slip','is_paid','is_shipped','user','is_active','postal_track','status')
         extra_kwargs = {'id':{'read_only':True}}
