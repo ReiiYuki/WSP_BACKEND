0a84cea8ed65adc614f9f07747a5962a6e6ad942
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-11-15 01:51:34 +0700

    Edit Design Models

--- naturedrinkbackend/design/models.py
+++ naturedrinkbackend/design/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class DesignBottle(models.Model) :
     name = models.CharField(max_length=1000)
     description = models.TextField()
     image = models.CharField(max_length=1000)
+    user = models.ForeignKey(User)
     is_active = models.BooleanField(default=True)
 
 class DesignRequest(models.Model) :
@@ -12,3 +13,4 @@ class DesignRequest(models.Model) :
     is_confirm = models.BooleanField(default=False)
     price = models.FloatField(default=None,null=True)
     is_active = models.BooleanField(default=True)
+    user = models.ForeignKey(User)
--- naturedrinkbackend/design/views.py
+++ naturedrinkbackend/design/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from .models import DesignBottle, DesignRequest
+from .serializers import DesignBottleSerializer,DesignRequestSerializer
+from rest_framework import viewsets,renderers,status
+from rest_framework.response import Response
+from rest_framework.decorators import list_route,detail_route
 
-# Create your views here.
+PERMISSION_DENIED_CONTENT = { "detail" : "Permission denied."}
+
+class DesignBottleViewSet(viewsets.ModelViewSet) :
+    queryset = DesignBottle.objects.all()
+    serializer_class = DesignBottleSerializer
+    
