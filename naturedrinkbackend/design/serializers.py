1964a92943bd249f1313997e7eb865d54decf244
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-11-15 01:03:34 +0700

    Design Serializer

--- naturedrinkbackend/design/serializers.py
+++ naturedrinkbackend/design/serializers.py
@@ -4,5 +4,10 @@ from rest_framework import serializers
 class DesignBottleSerializer(serializers.ModelSerializer) :
     class Meta :
         model = DesignBottle
-        fields = ('id','name','description','image')
-        extra_kwargs = {'id':{'read_only':True}}
+        fields = ('id','name','description','image','is_active')
+        extra_kwargs = {'id':{'read_only':True},'is_active':{'read_only':True}}
+class DesignRequestSerializer(serializers.ModelSerializer) :
+    class Meta :
+        model = DesignRequest
+        fields = ('id','bottle','is_confirm','price','is_active')
+        extra_kwargs = {'id':{'read_only':True},'is_active':{'read_only':True}}
