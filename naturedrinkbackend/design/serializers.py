ac1d5cc8b0d9119745cb2b14d32f06c36b96c2ad
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-11-19 18:37:07 +0700

    Fix bug on Design

--- naturedrinkbackend/design/serializers.py
+++ naturedrinkbackend/design/serializers.py
@@ -5,7 +5,7 @@ class DesignBottleSerializer(serializers.ModelSerializer) :
     class Meta :
         model = DesignBottle
         fields = ('id','name','description','image','is_active','user','is_request','is_confirm')
-        extra_kwargs = {'id':{'read_only':True},'is_active':{'read_only':True},'is_request':{'read_only':True},'is_confirm':{'read_only':True}}
+        extra_kwargs = {'id':{'read_only':True},'user':{'read_only':True},'is_active':{'read_only':True},'is_request':{'read_only':True},'is_confirm':{'read_only':True}}
 
 class BottleSerializer(serializers.ModelSerializer) :
     class Meta :
